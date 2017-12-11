62a849d0c293d0915d6be1eeca21ee1ce4311302
wangabeng <wangabeng@163.com>
2017-12-11 16:26:26 +0800

    add

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -15,11 +15,12 @@ export default {
 </script>
 
 <style lang='stylus' rel='stylesheet/stylus'>
+@import '~common/stylus/variable.styl'
 .chat
   position: absolute;
   left: 0;
   top: 0;
   width: 100%;
   height: 100%;
-  background-color: #f1f1f1;
+  background-color: $color-background;
 </style>
